321fb6d2bb94e770eb99db26e6cf4f9f26773160
Thiago Miranda <mirandathiago@gmail.com>
2023-10-25 15:57:14 -0300

    Nova Forma de Roteamento usando Array

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
 require_once "./vendor/autoload.php";
 
-use Ifba\Controller\ErroController;
-use Ifba\Controller\IndexController;
-
-
+$config = [];
+$config["controller"] = "\\Ifba\\Controller\\";
 
 
 $url = $_GET["url"] ?? "/";
 
+$rotas = [
+    "/" => ["IndexController","index"], 
+    "__erro404" => ["ErroController","erro404"]   
+];
 
 
-switch($url){
-    case "/":
-      $controller = new IndexController();
-      $controller->index();
-    break;
-    default:
-      $controller = new ErroController();
-      $controller->erro404();
+if( key_exists($url,$rotas) ){
+    [$controller,$metodo] = $rotas[$url];
     
+    carregarController($controller,$metodo);
+
+}else{
+    [$controller,$metodo] = $rotas["__erro404"];
+    carregarController($controller,$metodo);
+}
+
+
+function carregarController(string $controller,string $metodo){
+    global $config;
+    $controller =  $config["controller"].$controller;
+    $c = new $controller();
+    call_user_func_array([$c,$metodo],[]);
 }
